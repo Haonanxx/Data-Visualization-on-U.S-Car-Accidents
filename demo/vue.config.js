ac3abea860d6be9af2329d27e9e728d9f94a0315
tksjessie <70294231+tksjessie@users.noreply.github.com>
2020-11-12 16:52:31 -0800

    update as mentioned on Slack

--- demo/vue.config.js
+++ demo/vue.config.js
@@ -1,8 +1,11 @@
+const path = require('path')
+
 module.exports = {
     configureWebpack: {
         devtool: 'source-map'
-    },    
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/dsci-554-projects/team1/'  //production path
-      : '/'  //development path
-  }
+    },
+    publicPath: process.env.NODE_ENV === 'production' ?
+        path.resolve(__dirname,'/dsci-554-projects/project-YOUR_REPO_NAME') //production path
+        :
+        '/' //development path
+}
