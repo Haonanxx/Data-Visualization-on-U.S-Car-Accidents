3da94dac431ca2636f76c4e1549347eb6c17dd4b
Haonan Xu <haonanxu@haonans-mbp.lan>
2020-11-20 23:14:00 -0800

    Unsmoothy bar transition ver

--- demo/src/views/Weather.vue
+++ demo/src/views/Weather.vue
@@ -5,7 +5,6 @@
       <span class="sort" id="accidents">Sort by Number of Accidents</span>
       <span class="sort" id="temperature">Sort by Temperature</span>
       <span class="sort" id="visibility">Sort by Visibility</span>
-      <span class="sort" id="reset">Reset</span>
     </div>
     <svg width="1600" height="1600" id="chart"></svg>
   </div>
@@ -110,7 +109,7 @@ export default {
         if (mode === '#accidents') {
           this.chartData.sort((a, b) => d3.descending((a.accidents/a.population),(b.accidents/b.population)));
         } else if (mode === '#temperature') {
-          this.chartData.sort((a, b) => d3.descending(a.temperature, b.temperature));
+          this.chartData = this.chartData.sort((a, b) => d3.descending(a.temperature, b.temperature));
         } else if (mode === '#visibility') {
           this.chartData.sort((a, b) => d3.descending(a.visibility, b.visibility));
         }
@@ -133,6 +132,80 @@ export default {
 
       transition.selectAll('rect')
         .delay(delay);
+    },
+    redraw(){
+      var svg = d3.select('#chart')
+      svg.selectAll('rect').remove()
+      svg.selectAll('text').remove()
+      var sel = svg.selectAll('rect')
+        .data(this.chartData)
+        .enter();
+      sel.append('rect')
+        .attr('y', 50)
+        .attr('x', (d, i) => 20 + 3*i * 10)
+        .attr('height', d => d.accidents / d.population * 10000 + 100 )
+        .transition()
+        .duration(750)
+        .attr('class', 'bar3');
+      
+      sel.append('rect')
+        .attr('y', 50)
+        .attr('x', (d, i) => 20 + (3*i+1) * 10)
+        .attr('height', d => d.temperature * 4 )
+        .attr('class', 'bar');
+
+      sel.append('rect')
+        .attr('y', 50)
+        .attr('x', (d, i) => 20 + (3*i+2) * 10)
+        .attr('height', d => d.visibility * 100 - 750)
+        .attr('class', 'bar2');
+
+      sel.append('text')
+        .attr('y', 40)
+        .attr('x', (d, i) => 30 + (3*i+1) * 10)
+        .attr('class', 'label')
+        .text(d => d.state);
+
+      var legend = svg.selectAll('.legend')
+        .data(this.chartData)
+        .enter().append('g')
+        .attr('class', 'legend');
+      
+      legend.append('rect')
+        .attr('x', 1100)
+        .attr('y', 450)
+        .attr('width', 20)
+        .attr('height', 10)
+        .style('fill', '#7abcff');
+
+      legend.append('text')
+        .attr('x',1130)
+        .attr('y', 460)
+        .text('Average Temperature');
+      
+      legend.append('rect')
+        .attr('x', 1100)
+        .attr('y', 430)
+        .attr('width', 20)
+        .attr('height', 10)
+        .style('fill', '#e9c050');
+
+      legend.append('text')
+        .attr('x', 1130)
+        .attr('y', 440)
+        .text('Weighted Number (by population) of Accidents');
+
+      legend.append('rect')
+        .attr('x', 1100)
+        .attr('y', 470)
+        .attr('width', 20)
+        .attr('height', 10)
+        .style('fill', '#e95050');
+
+      legend.append('text')
+        .attr('x', 1130)
+        .attr('y', 480)
+        .text('Average Visibility');
     }
   },
   mounted: function () {
@@ -167,6 +240,29 @@ export default {
       this.barChart();
     }
 
+    d3.select('#temperature')
+      .on('click', () => {
+      this.sort('#temperature');
+      this.toggleSort('#temperature');
+      //this.transition();
+      this.redraw();
+    });
+
+    d3.select('#accidents')
+      .on('click', () => {
+      this.sort('#accidents');
+      this.toggleSort('#accidents');
+      //this.transition();
+      this.redraw();
+    });
+
+    d3.select('#visibility')
+      .on('click', () => {
+      this.sort('#visibility');
+      this.toggleSort('#visibility');
+      //this.transition();
+      this.redraw();
+    });
 
   }
 }
