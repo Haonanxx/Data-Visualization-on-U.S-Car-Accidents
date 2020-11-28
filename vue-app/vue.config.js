10cc569ff326402486fb32618b8359b599a0f275
tksjessie <70294231+tksjessie@users.noreply.github.com>
2020-11-28 07:00:23 -0800

    remove vue-app

--- vue-app/vue.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    configureWebpack: {
-        devtool: 'source-map'
-    },    
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/dsci-554-projects/team1/'  //production path
-      : '/'  //development path
-  }
