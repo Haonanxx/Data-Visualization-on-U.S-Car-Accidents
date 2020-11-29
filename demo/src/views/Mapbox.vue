bee2c51151fced4d337a3df44381a67acf4a1f3d
tksjessie <70294231+tksjessie@users.noreply.github.com>
2020-11-29 01:48:07 -0800

    add severity-infrastructure animated bar chart

--- demo/src/views/Mapbox.vue
+++ demo/src/views/Mapbox.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-
+    <div>
     <div class="left">
       <h3>Severity of traffic accidents of the states and counties in the US</h3>
       <div ref="basicMapbox" style="height:500px;width:800px;margin-left:50px" >
@@ -21,6 +21,31 @@
         <br>
         <svg id = "pie_chart" width="450" height="450"></svg>
       </div>
+</div>
+<div>
+  <div class="left">
+    <div>
+        <h3>The number of times the traffic accidents happened near the infrastructure in 49 states of the US from 2016 to 2020 with 4 different severities</h3>
+    </div>
+
+    <div id="chart"></div>
+  </div>
+<div class="right">
+  <br><br><br><br><br><br>
+  <div class="commands">
+        <span class="filter" id="all">All 10 infrastructures</span>
+    </div>
+    <div class="commands">
+        <span class="filter" id="top5">Top 5 infrastructures </span>
+    </div>
+    <br>
+    <div class="slidecontainer">
+      <input type="range" min="1" max="4" value="1" class="slider" id="myRange">
+      <div style="word-spacing: 45px">1 2 3 4</div>
+      <p>Severity: <span id="demo"></span></p>
+    </div>
+    </div>
+  </div>
   </div>
 </template>
 <script>
@@ -42,11 +67,373 @@ export default {
     }
   },
   mounted () {
-    // this.drawPieChart('West Virginia')
+    this.drawBar()
     this.init()
   },
   methods: {
-    // ,
+    drawBar(){
+    // margin convention
+var margin = { top: 20, left: 100, bottom: 50, right: 50 },
+width = 900 - margin.left - margin.right,
+height = 400 - margin.top - margin.bottom;
+
+var svg = d3.select('#chart').append('svg')
+.attr('width', width + margin.left + margin.right)
+.attr('height', height + margin.top + margin.bottom)
+.append('g')
+.attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
+
+// Declare scale function for x axis and y axis
+var x = d3.scaleBand();
+var y = d3.scaleLinear();
+
+// Declare delay time for index of data based on their indice
+var delay = function (d, i) {
+return i * 50;
+};
+
+// Declare variables
+var all, top5;
+var current, sortMode;
+
+// Load GDP.json with d3
+d3.json('severity_condition.json').then(data => {
+    
+    // const data = data
+    all = data.filter(d => {if (d.Severity == 1) return d})
+
+    //Set top for top 5 by values
+    top5 = all.sort(function(a,b){return d3.descending(+a.Value, +b.Value)})
+            .slice(0,5)
+
+            filter('#all');
+            sort('#descending_value');
+            
+            toggleFilter('#all');
+            // toggleSort('#descending_value');
+            
+            draw();
+
+    function update(severity_num){
+        all = data.filter(d => {if (d.Severity == severity_num) return d})
+// console.log(all);
+    //Set top for top 5 by values
+    top5 = all.sort(function(a,b){return d3.descending(+a.Value, +b.Value)})
+            .slice(0,5)
+
+            filter('#all');
+            sort('#descending_value');
+            
+            toggleFilter('#all');
+            // toggleSort('#descending_value');
+            
+            redraw();
+    }
+    
+
+    var slider = document.getElementById("myRange");
+    var output = document.getElementById("demo");
+    
+    output.innerHTML = slider.value;
+    // console.log(slider.value);
+    slider.oninput = function() {
+      output.innerHTML = this.value;
+    //   console.log(this.value);
+    update(slider.value)
+    // console.log(slider.value);
+    
+      
+    }
+
+
+
+// initial settings (all 10 countries ordered alphabetic by name) before clicking on any button
+
+});
+
+// reset event handlers, reset to sort alphabetically by name and filter all by clicking the reset button
+d3.select('#reset')
+.on('click', () => {
+// reset to initial settings (all 10 countries ordered alphabetic by name)
+filter('#all');
+sort('#descending_value');
+
+// change highlight button
+// toggleSort('#descending_value');
+toggleFilter('#all');
+// toggleReset('#reset')
+
+redraw();
+});
+
+
+//implement click events to sort descendingly by value
+d3.select('#descending_value')
+.on('click', () => {
+sort('#descending_value');
+transition();
+// toggleSort('#descending_value');
+});
+
+
+//filter event handlers
+//implement click events to show all
+d3.select('#all')
+.on('click', () => {
+
+
+filter('#all');
+
+
+sort(sortMode);
+
+// toggleSort(sortMode);
+toggleFilter('#all');
+
+redraw();
+});
+
+//implement click events to show top 5 by value
+d3.select('#top5')
+.on('click', () => {
+
+filter('#top5');
+sort(sortMode);
+
+// toggleSort(sortMode);
+toggleFilter('#top5');
+
+redraw();})
+
+
+// define filtering function
+function filter(mode) {
+
+
+if (mode === '#all') {
+    current = JSON.parse(JSON.stringify(all));
+} else if (mode === '#top5') {
+    current = JSON.parse(JSON.stringify(top5));
+} 
+// filterMode = mode;
+}
+
+// define sorting function
+function sort(mode) {
+
+    current.sort((a, b) => d3.descending(+a.Value, +b.Value));
+
+//Sorted country names as the discrete domain of x scaler. The positions of them on x axis may change after sorting in current, so map again.
+x.domain(current.map(d => d.Condition));
+sortMode = mode;
+
+}
+
+
+
+
+//define function to highlight the toggled sorting label
+function toggleFilter(id) {
+d3.select('.init')
+    .style('background-color', '#FB6E51')
+    .style("cursor", "pointer");
+d3.selectAll('.filter')
+    .style('background-color', '#ffcdb2')
+    .style("cursor", "pointer");
+
+d3.select(id)
+    .style('background-color', '#FB6E51')
+    .style("cursor", "pointer");
+}
+
+//Define function to draw the initial bar chart
+function draw() {
+//var x = d3.scaleBand();
+//Map the countries in current as the discrete domain of the axis in the translated group
+x.domain(current.map(d => d.Condition))
+    .range([0, width])
+    .paddingInner(0.3)
+    .paddingOuter(0.3); //add outer padding to scaleBand
+
+
+//var y = d3.scaleLinear();
+// console.log(height)
+y.domain([0, d3.max(current, d => +d.Value)])
+    .range([height, 0])
+    .nice();
+
+// Draw bars
+    // The x coordernate of the star point of each bar is the value mapping from the discrete domain.
+    // y(d.Value) is  the value in the range mapping from Value of the domain of y
+    // bandwidth() is the width of the scaled bar by d3.scaleBand
+svg.selectAll('.bar')
+    .data(current, d => d.Condition)
+    .enter()
+    .append('rect')
+    .attr('class', 'bar')
+    .attr('x', d => x(d.Condition))
+    .attr('y', d => y(+d.Value))
+    .attr('width', x.bandwidth())
+    .attr('height', d => height - y(+d.Value));
+
+//Draw x label
+    // Draw labels for x axis
+svg.selectAll('.name')
+    .data(current, d => d.Condition)
+    .enter()
+    .append('text')
+    .text(d => d.Name)
+    .attr('class', 'name')
+    .attr('x', d => x(d.Condition) + x.bandwidth() / 2)
+    .attr('y', d => y(+d.Value) + (height - y(+d.Value)) +20 );
+// Draw x axis at the bottom
+var xAxis;
+xAxis = d3.axisBottom()
+    .scale(x)
+    .ticks(0)
+    .tickSize(0)
+    .tickFormat('');
+// Append the group translate height in vertical from the position translated last time, with ticks on the bottom then draw x axis 
+svg.append('g')
+    .attr('class', 'x-axis')
+    .attr('transform', 'translate(0,' + height + ')')
+    .call(xAxis);
+
+//draw y axis with ticks on the left
+var yAxis = d3.axisLeft()
+    .scale(y)
+    .ticks(5, 'd');
+
+svg.append('g')
+    .attr('class', 'y-axis')
+    .call(yAxis);
+
+svg.append('text')
+    .attr('x', - height / 2)
+    .attr('y', - margin.left * 0.7)
+    .attr('transform', 'rotate(-90)')
+    .attr('class', 'ylabel')
+    .append('tspan').text('Number of times')
+    .style('baseline-shift', 'super')
+    .style('font-size', '15px');
+}
+
+//define function for transition
+function transition() {
+var transition = svg.transition()
+    .duration(1000);
+
+transition.selectAll('.bar')
+    .delay(delay)
+    .attr('x', d => x(d.Condition));
+
+transition.selectAll('.name')
+    .delay(delay)
+    .attr('x', d => x(d.Condition) + x.bandwidth() / 2);
+}
+
+//Redraw because the data change, the number of bars and x labels become smaller after filtering, so the shapes of bars change after rescaling. 
+function redraw() {
+    //update scale
+    x.domain(current.map(d => d.Condition))
+
+    var xAxis = d3.axisBottom()
+    .scale(x)
+    .ticks(0)
+    .tickSize(0)
+    .tickFormat('');
+// Append the group translate height in vertical from the position translated last time, with ticks on the bottom then draw x axis 
+svg.select('.x-axis')
+    .attr('transform', 'translate(0,' + height + ')')
+    .call(xAxis);
+
+    y.domain([0, d3.max(current, d => +d.Value)])
+    .range([height, 0])
+    .nice();
+
+
+//draw y axis with ticks on the left
+var yAxis = d3.axisLeft()
+    .scale(y)
+    .ticks(5, 'd');
+
+d3.select('.y-axis')
+    .transition()  
+    .duration(1000)  
+    .call(yAxis);
+
+
+// data join for bars
+var bars = svg.selectAll('.bar')
+    .data(current, d => d.Condition);
+
+// update
+bars.transition()
+    .duration(1000)
+    .delay(delay)
+    .attr('x', d => {
+        // console.log(x(d.Condition));
+        return x(d.Condition)})
+    .attr('width', x.bandwidth());
+
+// exit
+bars.exit()
+    .transition()  
+    .duration(1000)  
+    .style('opacity', 0)  
+    .remove();
+
+// enter
+bars.enter()
+    .append('rect')
+    .attr('x', d => x(d.Condition))  
+    .attr('y', y(0))  
+    .attr('width', x.bandwidth())  
+    .transition()  
+    .duration(1000)  
+    .attr('class', 'bar')  
+    .attr('x', d => x(d.Condition))  
+    .attr('y', d => {
+        // console.log(y(+d.Value));
+        return y(+d.Value)})  
+    .attr('width', x.bandwidth())  
+    .attr('height', d => height - y(+d.Value));  
+
+// DATA JOIN FOR NAMES.
+var name = svg.selectAll('.name')
+    .data(current, d => d.Condition);
+
+// UPDATE.
+name.transition()
+    .duration(1000)
+    .delay(delay)
+    .attr('x', (d) => x(d.Condition) + x.bandwidth() / 2);
+
+// ENTER.
+name.enter()
+    .append('text')
+    .attr('x', d => x(d.Condition) + x.bandwidth() / 2)
+    .attr('y', d => y(+d.Value) + (height - y(+d.Value)) +20 )
+    .style('opacity', 0)
+    .transition()
+    .duration(1000)
+    .text(d => d.Name)
+    .attr('class', 'name')
+    .attr('x', d => x(d.Condition) + x.bandwidth() / 2)
+    .attr('y', d => y(+d.Value) + (height - y(+d.Value)) +20 )
+    .style('opacity', 1);
+
+// EXIT.    
+name.exit()
+    .transition()
+    .duration(1000)
+    .style('opacity', 0)
+    .remove();
+
+}
+}
+
+,
     // initialize
     init () {
       // const vue = this
@@ -73,8 +460,8 @@ export default {
                 'Station', 'Stop', 'Traffic_Signal']
       var oldPieData = []
       var filteredPieData = []
-      console.log(oldPieData);
-      console.log(filteredPieData);
+      // console.log(oldPieData);
+      // console.log(filteredPieData);
       drawPieChart("total")
       function drawPieChart(state_name){
               d3.json('state_road_condition.json').then(jsondata => {
@@ -173,7 +560,7 @@ export default {
                   function pieTween(d, i) {
                       var start_angel;
                       var end_angel;
-                      console.log(d);
+                      // console.log(d);
                       //   console.log(i);
                       if (oldPieData[i]) {
                             // console.log(oldPieData[i]);
@@ -357,10 +744,13 @@ export default {
           var total = document.getElementById('total');
           var title_state = document.createElement('strong');
             title_state.textContent = 'the US'
+            title_state.style.color="red"
+
           pie_title.innerHTML = '';
           pie_title.textContent =
             'The percentage of road conditions of traffic accidents in ';
-            
+            pie_title.style.fontSize = "18.72px"
+            pie_title.style.fontWeight = "bold"
             pie_title.appendChild(title_state);
 
           // var tot = document.createElement('button');
@@ -370,9 +760,12 @@ export default {
           map.setFilter('click_state_highlighted', ['==', 'id', '']);
           var title_state = document.createElement('strong');
             title_state.textContent = 'the US'
+            title_state.style.color="red"
           pie_title.innerHTML = '';
           pie_title.textContent =
             'The percentage of road conditions of traffic accidents in ';
+             pie_title.style.fontSize = "18.72px"
+            pie_title.style.fontWeight = "bold"
             pie_title.appendChild(title_state);
           
           });
@@ -384,9 +777,12 @@ export default {
           drawPieChart(feature.properties.name)
           var title_state = document.createElement('strong');
             title_state.textContent = feature.properties.name.toLocaleString()
+            title_state.style.color="red"
           pie_title.innerHTML = '';
           pie_title.textContent =
             'The percentage of road conditions of traffic accidents in ';
+             pie_title.style.fontSize = "18.72px"
+            pie_title.style.fontWeight = "bold"
             pie_title.appendChild(title_state);         
           });
 
@@ -606,4 +1002,123 @@ width: 10px;
   padding: 20px 25px;
   margin-top: 20px;
 }
+
+.axis path,
+        .axis line {
+            fill: none;
+            stroke: black;
+            shape-rendering: crispEdges;
+        }
+
+        .axis text {
+            font-family: Courier;
+            font-size: 0.85em;
+        }
+
+        text {
+            font-family: Courier;
+            font-size: 0.65em;
+        }
+
+        div.commands {
+            font-family: Courier;
+            font-size: 0.85em;
+            font-weight: bold;
+            text-align: center;
+            cursor: default;
+            user-select: none;
+        }
+
+        .bar {
+            fill: #FB6E51;
+        }
+
+        text.xlabel {
+            text-anchor: middle;
+        }
+
+        text.ylabel {
+            font-size: 2em;
+            text-anchor: middle;
+            alignment-baseline: central;
+        }
+
+        text.name {
+            font-weight: bold;
+            text-anchor: middle;
+            alignment-baseline: central;
+        }
+
+        .sort {
+            border-radius: 3px;
+            background-color: #eee;
+            display: inline-block;
+            cursor: default;
+        }
+
+        .sort,
+        label {
+            font-family: Courier;
+            color: #444;
+            padding: 5px;
+            margin: 5px;
+        }
+
+        label {
+            vertical-align: text-bottom;
+        }
+
+        .filter {
+            border-radius: 3px;
+            background-color: #eee;
+            padding: 5px;
+            margin: 5px;
+            color: #444;
+            text-align: left;
+            display: inline-block;
+            cursor: default;
+        }
+
+        .init {
+            border-radius: 3px;
+            background-color: #eee;
+            padding: 5px;
+            margin: 5px;
+            color: #444;
+            text-align: left;
+            display: inline-block;
+            cursor: default;
+        }
+        .slider {
+            -webkit-appearance: none;
+            width: 190px;
+            height: 15px;
+            border-radius: 5px;  
+            background: #d3d3d3;
+            outline: none;
+            opacity: 0.7;
+            -webkit-transition: .2s;
+            transition: opacity .2s;
+          }
+          
+          .slider::-webkit-slider-thumb {
+            -webkit-appearance: none;
+            appearance: none;
+            width: 25px;
+            height: 25px;
+            border-radius: 50%; 
+            background: #4CAF50;
+            cursor: pointer;
+          }
+          
+          .slider::-moz-range-thumb {
+            width: 25px;
+            height: 25px;
+            border-radius: 50%;
+            background: #4CAF50;
+            cursor: pointer;
+          }
+          .y-axis{
+            font-size: 24px;
+          }
 </style>
